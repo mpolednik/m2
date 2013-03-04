cfb78e2847dbd8e753ddb4a6715ddc3296e0e606
Martin Polednik <setraha@gmail.com>
2013-03-04 09:12:32 +0100

    Request controller functionality for submitting new requests, mock accept (TODO: decline, accept functionality)

--- app/controllers/request.py
+++ app/controllers/request.py
@@ -1,7 +1,18 @@
+from flask import request, session, flash, redirect, url_for
+
 from app.helpers.middleware import db
+from app.helpers.bootstrap import current_user
 from app.helpers.rendering import render
 
+from flask.ext.wtf import Form
+from wtforms import fields, validators
+
 from app.models.request import Request
+
+class RequestForm(Form):
+    name = fields.TextField('Jmeno')
+    text = fields.TextAreaField('Text')
+    type = fields.SelectField('Typ', choices=[('0', 'Category'), ('1', 'Moderator')])
 
 def request_all():
     requests = db.session.query(Request).all()
@@ -13,5 +24,18 @@ def request_one(id):
 
     return render('request.html', request=request)
 
-def request_new():
+def request_submit(category = None):
+    form = RequestForm()
+
+    if request.method == 'POST' and form.validate():
+        req = Request(current_user, None, form.type.data, form.name.data, form.text.data)
+        db.session.add(req)
+        db.session.commit()
+        flash('request odeslan')
+        return redirect('/')
+
+    return render('request_form.html', form=form)
     return 'In progress...'
+
+def request_accept(request):
+    pass
