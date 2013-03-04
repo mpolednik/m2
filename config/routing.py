4e9e29ff45480d0925ae33060c831c0f773d4156
Martin Polednik <setraha@gmail.com>
2013-03-04 09:07:03 +0100

    Routes for new existing and future controllers

--- config/routing.py
+++ config/routing.py
@@ -2,8 +2,21 @@ from app.helpers.middleware import views
 
 views.add('/', 'app.controllers.category.category_all')
 views.add('/c/<name>', 'app.controllers.category.category_one')
-views.add('/i/<id>', 'app.controllers.image.image')
-views.add('/u/<id>', 'app.controllers.user.user')
+views.add('/c/<name>/submit', 'app.controllers.category.category_submit', methods=('GET', 'POST'))
+views.add('/c/<name>/edit', 'app.controllers.category.category_edit', methods=('GET', 'POST'))
+
+views.add('/i/<id>', 'app.controllers.image.image', methods=('GET', 'POST'))
+views.add('/i/<id>/edit', 'app.controllers.image.image_edit', methods=('GET', 'POST'))
+views.add('/i/<id>/delete', 'app.controllers.image.image_delete')
+views.add('/i/<id>/vote', 'app.controllers.image.image_vote')
+
+views.add('/i/<id>/c/<cid>', 'app.controllers.comment.comment', methods=('GET', 'POST'))
+views.add('/i/<id>/c/<cid>/edit', 'app.controllers.comment.comment_edit', methods=('GET', 'POST'))
+views.add('/i/<id>/c/<cid>/delete', 'app.controllers.comment.comment_delete')
+views.add('/i/<id>/c/<cid>/vote', 'app.controllers.comment.comment_vote')
+
 views.add('/r', 'app.controllers.request.request_all')
-views.add('/r/new', 'app.controllers.request.request_new')
 views.add('/r/<id>', 'app.controllers.request.request_one')
+views.add('/r/submit', 'app.controllers.request.request_submit', methods=('GET', 'POST'))
+
+views.add('/u/<id>', 'app.controllers.user.user')
