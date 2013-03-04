ee6b173dd565de6f437d9d04b17fcb48eac86965
Martin Polednik <setraha@gmail.com>
2013-03-04 09:07:41 +0100

    Category controller now supports category_submit and category_edit

--- app/controllers/category.py
+++ app/controllers/category.py
@@ -1,8 +1,23 @@
+from flask import redirect, url_for, request
+
 from app.helpers.middleware import db, app
 from app.helpers.rendering import render
+from app.helpers.bootstrap import current_user
+
+from flask.ext.wtf import Form
+from wtforms import fields, validators
 
 from app.models.image import Image
 from app.models.category import Category
+
+class SubmitForm(Form):
+    name = fields.TextField('Jmeno')
+    text = fields.TextAreaField('Text')
+    path = fields.TextAreaField('Path')
+    file = fields.FileField('Image')
+
+class EditForm(Form):
+    text = fields.TextAreaField('Text')
 
 def category_all():
     images = db.session.query(Image).all()
@@ -14,3 +29,23 @@ def category_one(name):
     images = db.session.query(Image).filter_by(category=category).all()
 
     return render('category.html', title='test', category=category, images=images)
+
+def category_submit(name):
+    form = SubmitForm(request.form)
+
+    category = db.session.query(Category).filter_by(name=name).one()
+
+    if request.method == 'POST' and form.validate():
+        image = Image(current_user, category, form.name.data, form.text.data, form.path.data)
+        db.session.add(image)
+        db.session.commit()
+        return redirect('/c/{}'.format(name))
+
+    return render('category_submit.html', title='test', form=form)
+
+def category_edit(name):
+    form = SubmitForm()
+
+    category = db.session.query(Category).filter_by(name=name).one()
+
+    return render('category_edit.html', title='test', form=form, category=category)
