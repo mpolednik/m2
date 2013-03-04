747ca486e11380fc90ed747cde5c54889326e19b
Martin Polednik <setraha@gmail.com>
2013-03-04 09:13:55 +0100

    Implemented algorithm to return list containing comments sorted as they should be

--- app/models/comment.py
+++ app/models/comment.py
@@ -9,6 +9,7 @@ class Comment(db.Model):
     id_image= db.Column(db.Integer, db.ForeignKey('image.id'))
     text = db.Column(db.String(1000))
     rating = db.Column(db.Integer, default=0)
+    state = db.Column(db.Integer, default=0)
     ts = db.Column(db.DateTime)
 
     father = db.relationship('Comment')
@@ -21,3 +22,21 @@ class Comment(db.Model):
 
     def __repr__(self):
         return '<Comment({}, {}, {}, {}, {}, {}, {})>'.format(self.id, self.owner, self.id_father, self.id_image, self.text, self.rating, self.ts)
+
+def construct_comment_tree(comments, father = None):
+    tree = []
+
+    for comment in comments:
+        if comment.id == father or father is None and comment.id_father == father:
+            comment.depth = 1
+            tree.append(comment)
+            _find_nodes(2, tree, comment, comments)
+
+    return tree
+
+def _find_nodes(depth, tree, father, comments):
+    for comment in comments:
+        if comment.id_father == father.id:
+            comment.depth = depth
+            tree.append(comment)
+            _find_nodes(depth+1, tree, comment, comments)
