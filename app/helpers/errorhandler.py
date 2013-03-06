8eb06598c116bb12d0fef71a672fbd230332c175
Martin Polednik <setraha@gmail.com>
2013-03-06 12:53:50 +0100

    Application errors are now caught and handled

---
+++ app/helpers/errorhandler.py
@@ -0,0 +1,10 @@
+from app.helpers.middleware import app
+from app.helpers.rendering import render
+
+@app.errorhandler(404)
+def page_error(error):
+    return render('errors/404.html')
+
+@app.errorhandler(500)
+def page_error(error):
+    return render('errors/500.html', error=error)
--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from app.helpers.middleware import app
 
+from app.helpers.errorhandler import *
 import config.routing
 
 if __name__ == '__main__':
