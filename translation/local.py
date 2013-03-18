9a2ecd5f9a3d31609d770f09bb122073abdccd45
Martin Polednik <setraha@gmail.com>
2013-03-18 13:33:43 +0100

    Added pass_mod and become_mod methods, form validation and error flashing

---
+++ translation/local.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+NAME=u'Jméno'
+TEXT=u'Popis'
+LINK=u'Odkaz'
+IMAGE=u'Obrázek'
+PASS=u'Heslo'
+MAIL=u'E-mail'
+UNAME=u'Přezdívka'
+PHONE=u'Telefonní číslo'
+CONTENT=u'Obsah'
+
+category_submit = {
+    'INVALID_NAME': u'Délka názvu obrázku musí být mezi %(min)d a %(max)d znaky!',
+    'INVALID_TEXT': u'Maximální délka textu je %(max)d znaků!',
+    'INVALID_LINK': u'Maximální délka odkazu je %(max)d znaků!',
+}
+
+user = {
+    'INVALID_MAIL': u'Neplatná e-mailový adresa! E-mail musí být zadán ve formátu name@domain.tld.',
+    'INVALID_PASS': u'Heslo musí mít minimálně %(min)d znaků!',
+    'INVALID_NAME': u'Délka přezdívky musí být mezi %(min)d a %(max)d znaky!',
+    'EXISTS_MAIL': u'Tato e-mailová adresa je již zaregistrována!',
+    'EXISTS_NAME': u'Tato přezdívka je již zaregistrována!',
+}
+
+comment = {
+    'INVALID_TEXT': u'Délka obsahu musí být mezi %(min)d a %(max)d znaky!',
+}
