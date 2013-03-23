6e2d3c36cb21f1e76c8cb3a01b6c3aede629269e
Martin Polednik <setraha@gmail.com>
2013-03-23 11:18:54 +0100

    Few design upgrades

--- static/js/generic.js
+++ static/js/generic.js
@@ -1,9 +1,19 @@
-$('.flashes').delay(2500).fadeOut(300)
+// Fade flashes 2.5 secs after page load
+$('.flashes').delay(2500).fadeOut(300);
 
-$(function(){
-  $('.row').masonry({
-    // options
-    itemSelector : '.unit',
-    columnWidth : 240
+// Highlight images on mouse oever
+$('.item').mouseover(function () {
+    $(this).css('background-color', '#ccc'); 
+    $(this).css('border', '1px solid #adadad'); 
+}).mouseout(function () {
+    $(this).css('background-color', '#efefef'); 
+    $(this).css('border', '1px solid #e7e7e7'); 
+});
+
+// Initiate masonry
+var $container = $('.masonry');
+$container.imagesLoaded(function(){
+  $container.masonry({
+    itemSelector : '.item',
   });
 });
